50931658714c0f339cb67f43e1f104eecf81bd03
Andrew Rodney <drew_55g@yahoo.com>
2013-06-27 13:59:04 -0400

    Initial Commit

---
+++ Jun27/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Jun27
+//
+//  Created by andrew rodney on 6/27/13.
+//  Copyright (c) 2013 andrew rodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Jun27/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Jun27
+//
+//  Created by andrew rodney on 6/27/13.
+//  Copyright (c) 2013 andrew rodney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
